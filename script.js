f715ecb9898332b0f127a1515f7aec662149cf71
Ben Lynch <bdlynch7@gmail.com>
2023-07-11 16:50:55 -0500

    finished addition checkpoint

---
+++ script.js
@@ -0,0 +1,15 @@
+// addition checkpoint
+function sumOfTwo (num1, num2) {
+console.log("in <sumOfTwo>");
+return num1 + num2;
+}
+
+// testing sumOfTwo
+let test1 = sumOfTwo(2, 3);
+console.log("should be 5:", test1);
+
+let test2 = sumOfTwo(2, 7);
+console.log("should be 9:", test2);
+
+
+// conditional checkpoint
