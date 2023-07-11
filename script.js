1ad59e74c437b57ca64ee3c0f9b321d8d888c48a
Ben Lynch <bdlynch7@gmail.com>
2023-07-11 16:59:20 -0500

    conditional checkpoint

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ console.log("should be 9:", test2);
 
 
 // conditional checkpoint
+function toCompare (num1, num2) {
+    console.log("in <toCompare>");
+    if (num1 > num2) {
+        return "The first number was bigger!"
+    }
+    if (num1 < num2) {
+        return "The second number was bigger!"
+    }
+    if (num1 == num2) {
+        return "The numbers are the same!"
+    }
+}
+
+// testing toCompare, num1 > num2
+console.log(toCompare (3, 2));
+// testing toCompare, num1 < num2
+console.log(toCompare (2, 7));
+// testing toCompare, num1 = num2
+console.log(toCompare (7, 7));
